00532b937cf1d7c8fd53370e2f0fb47a12af5fc5
mandeep704 <57005384+mandeep704@users.noreply.github.com>
2020-03-25 21:45:33 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+</script>
+<![endif]-->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Demo Page</title>
+        
+        <style>
+        *{
+box-sizing: border-box;
+}
+#wrapper{
+    width:80%;
+    margin:auto;
+    background-color:#f0f0f0;
+    border:2px solid black;
+    box-shadow: 3px 4px;
+    overflow:hidden;
+    
+}
+#logo{
+  margin-left: 40px;
+   
+}
+
+header{
+    text-align: center;
+    overflow:hidden;
+}
+header img{
+    float:left;
+   
+    margin:1px;
+}
+h1{
+    color:hotpink;
+    font-family: cursive;
+  
+    text-shadow: 2px 2px #003366;}
+
+
+nav{
+    width:100%;
+     height:70px;
+     overflow:hidden;
+    background-color:#FF69B4;
+    text-align: center;
+    font-family: cursive;
+    
+}
+nav ul li{
+     width: 149px;
+    box-sizing: border-box;
+    overflow: visible;
+    list-style-type: none;
+    display: inline;
+    
+    
+}
+#tit{
+    color: hotpink;
+    font-family: cursive;
+    text-align: center;
+  font-weight: bold;
+    font-size: 2.4em;
+    text-shadow: 1px 1px #191970;
+}
+
+h3{
+    color: hotpink;
+    font-family: cursive;
+    text-align: center;
+  font-weight: bold;
+    font-size: 2.4em;
+    text-shadow: 1px 1px #191970;
+    background-color: pink;
+    height: 70px;
+
+}
+.foot ul li a{
+    color:white;
+    text-decoration: none;
+}
+
+#i{
+    text-align: center;
+  
+}
+            li .a{
+    padding:55px;
+    color:white;
+    text-decoration: none;
+    font-size:1.4em;
+    font-weight: bold;
+    text-align:center;
+}
+li .a:link{
+    color: #eee;
+
+}
+li .a:hover{
+    color: #000;
+    font-size:1.5em;
+
+}
+.container{
+    
+   margin-left: 100px;
+    margin-right: 100px;
+    overflow:hidden;
+   margin-top:10px;
+    font-family: cursive;
+    
+}
+img{
+    padding:5px;
+    padding-top:10px;
+    padding-bottom:7px;
+}
+
+h2{
+    text-align: center;
+    height:auto;
+    width:95%;
+    position:relative;
+    color:white;
+    background-color:lightpink;
+    padding-left:1.3em;
+    padding-right:0;
+    margin-right:0;
+    line-height: normal;
+    margin-left:0.5em;
+    letter-spacing: normal;
+}
+footer{
+    margin-top:14px;
+    text-align: center;
+    overflow:hidden;
+  background-color: hotpink;
+    width: 100%;
+    height: 100px;
+    
+}
+
+.foot{
+    color: white;
+    font-family: cursive;
+}
+
+
+        </style>
+        
+    </head>
+    <body>
+        <div id="wrapper">
+             <header>
+                 <div id="logo">   <img src="loggo.png" alt="Logo" width="80" height="80"></div>
+                <h1>Beauty Salon</h1>
+            </header>     
+            <nav>
+                    <ul>
+                        <li><a href="index.html" class="a">Home</a></li>
+                        <li><a href="services.html" class="a">Services</a></li>
+                        <li><a href="seminor.html" class="a">Seminars</a></li>
+                        <li><a href="contact.html" class="a">Contact Us</a></li>
+                        
+                    </ul>
+                
+            </nav>
+            <br>
+            <main>
+            
+                <div class="container">
+                Mandeep Kaur Demo site
+            </div>
+            
+            </main>
+            <footer>
+                
+            <p> <strong>Copyright&copy; salon</strong> </p>
+                
+               
+            </footer>
+        </div>
+    
+    </body>
+
+
+</html>
